9da0ce96d862a2b4b192e2b44662f2f64744b556
Роман Данилевич <dwordchanel@gmail.com>
2020-05-19 00:48:00 +0300

    Add project files.

---
+++ OOP_lab_3_2_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OOP_lab_3_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int k = 0;
+
+            foreach (char c in text)
+            {
+                if (Char.IsDigit(c))
+                {
+                    ++k;
+                }
+            }
+
+            string[] words = text.Split(new char[] { '\n', '\r', ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*', '/', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string tempStr = "бвгґджзйклмнпрстфхцчшщьbcdfghjklmnpqrstvwxz";
+
+            char[] ch = tempStr.ToCharArray();
+
+            Console.Write("\nCлова, що починаються з приголосних лiтер: ");
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < ch.Length; ++i)
+                {
+                    if (word[0] == ch[i])
+                    {
+                        Console.Write("{0} ", word);
+
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nKiлькiсть цифр у текстi: {0}", k);
+        }
+    }
+}
